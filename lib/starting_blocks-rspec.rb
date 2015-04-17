22799cd95d653af6022051d7bf8aaa120e7637fd
Darren Cauthon <darren@cauthon.com>
2015-04-17 11:49:47 -0500

    Trying patching even higher.

--- lib/starting_blocks-rspec.rb
+++ lib/starting_blocks-rspec.rb
@@ -2,5 +2,10 @@ require 'starting_blocks'
 require_relative 'rspec_contract'
 require_relative 'text_parser_override'
 
-puts 'gotcha'
-class IO; def tty?; true; end; end
+puts 'gotcha2'
+class IO; def tty?; puts 'true true true'; true; end; end
+RSpec::Core.instance_eval do
+  def output_to_tty? * args
+    true
+  end
+end
