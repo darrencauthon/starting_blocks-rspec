60a821a5fe65f3ba8673d66985ae76b5e2b721e2
Darren Cauthon <darren@cauthon.com>
2015-04-17 11:57:28 -0500

    Ugh.

--- lib/starting_blocks-rspec.rb
+++ lib/starting_blocks-rspec.rb
@@ -2,10 +2,4 @@ require 'starting_blocks'
 require_relative 'rspec_contract'
 require_relative 'text_parser_override'
 
-puts 'gotcha2'
-class IO; def tty?; puts 'true true true'; true; end; end
-RSpec::Core.instance_eval do
-  def output_to_tty? * args
-    true
-  end
-end
+#class IO; def tty?; puts 'true true true'; true; end; end
